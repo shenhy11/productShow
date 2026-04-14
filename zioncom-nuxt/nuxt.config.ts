c7e0fd8cc828bb1ce5af9c7bc6883ff116ad272d
huanyueshen <8544714+shen95@user.noreply.gitee.com>
2026-04-14 21:57:17 +0800

    fix(nuxt): 降级 Nuxt 至 3.15 + 升级 i18n 至 9.x，修复 unhead getActiveHead 兼容性问题

--- zioncom-nuxt/nuxt.config.ts
+++ zioncom-nuxt/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
   app: {
