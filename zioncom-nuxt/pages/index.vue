e2a0927318db614d0b6a16022222645f8cd02172
huanyueshen <8544714+shen95@user.noreply.gitee.com>
2026-04-14 12:20:31 +0800

    feat(前端): 完成任务 10 - 组装首页 components 并在 pages/index.vue 输出

---
+++ zioncom-nuxt/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <HomeHeroBanner />
+    <HomeQuickLinks />
+    <HomeProductCategories />
+    <HomeLatestNews />
+  </div>
+</template>
+
+<script setup>
+// import 结构说明：Nuxt3 默认开启自动导入 components 的规则
+// 由于我们将其放置在 /components/home/HeroBanner.vue
+// 在模板中你可以直接使用 <HomeHeroBanner />
+
+useHead({
+  title: 'Home',
+  meta: [
+    { name: 'description', content: 'Zioncom Home - Leading provider of networking devices.' }
+  ]
+})
+</script>
+
+<style scoped>
+</style>
